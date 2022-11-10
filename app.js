2b40d767d1cb36ad9b2007db00d9d05e6033d2cc
Cris <mccrys_00@yahoo.com>
2022-11-10 15:24:29 +0100

    Created social status functionality using hardcoded values

--- app.js
+++ app.js
@@ -18,13 +18,14 @@ const io = new Server(server, {
 });
 
 
-function getFriendsStatusAndNotifyFriends(USER_SOCKET_ROOM, FRIENDS_LIST, socket) {
+function getFriendsStatusAndNotifyFriends(USER_SOCKET_ROOM, FRIENDS_LIST) {
   FRIENDS_LIST.forEach(friend => {
     // get friend's socket room
     const friendSocketRoom = 'user__' + friend.socketRoom;
 
     // if the friend's room exists it means the friend is online
-    if (io.sockets.adapter.rooms.get(friendSocketRoom)) {
+    const friendRoom = io.sockets.adapter.rooms.get(friendSocketRoom);
+    if (friendRoom) {
       // emit to friend that user is on
       io.to(friendSocketRoom).emit("update_user_status", USER_SOCKET_ROOM, 'on');
 
@@ -38,50 +39,40 @@ function getFriendsStatusAndNotifyFriends(USER_SOCKET_ROOM, FRIENDS_LIST, socket
 }
 
 io.on("connection", (socket) => {
-  console.log('=========NEW CONNECTION==========')
+  // console.log('\n\n=========NEW CONNECTION==========')
   let USER_SOCKET_ROOM = null;
   let FRIENDS_LIST = null;
 
   socket.on("user_connected", (userAndFriendsList) => {
-    // simulate the connectivity of other friends
-
     USER_SOCKET_ROOM = 'user__' + userAndFriendsList.socketRoom;
     FRIENDS_LIST = userAndFriendsList.friendsList;
 
-    //each user creates an unique room number based on UUID sent from database
+    //the user creates an unique room number based on UUID sent from database which
+    // is joined by each separate connection of the same user
     socket.join(USER_SOCKET_ROOM)
-    console.log("on_connection:", io.sockets.adapter.rooms)
 
     // get list of friends with statuses and notify active friends that the user is online
-    const friendsList = getFriendsStatusAndNotifyFriends(USER_SOCKET_ROOM, FRIENDS_LIST, socket);
-    // console.log(friendsList)
+    const friendsList = getFriendsStatusAndNotifyFriends(USER_SOCKET_ROOM, FRIENDS_LIST);
     // emit to user the list of friends with statuses
     socket.emit("friends_status", friendsList);
   });
 
   socket.on("disconnect", () => {
-    // console.log('USER_SOCKET_ROOM', USER_SOCKET_ROOM)
     if (FRIENDS_LIST) {
       FRIENDS_LIST.forEach(friend => {
         // get friend's socket room
         const friendSocketRoom = 'user__' + friend.socketRoom;
-        
+      
         // notify only the active friends
         if (io.sockets.adapter.rooms.get(friendSocketRoom)) {
-          // emit to friend that user is disconnected
-          io.to(friendSocketRoom).emit("update_user_status", USER_SOCKET_ROOM, 'off');
+          // emit to friend that user is disconnected only if user has all sockets(connections) closed
+          if (!io.sockets.adapter.rooms.has(USER_SOCKET_ROOM)) {
+            io.to(friendSocketRoom).emit("update_user_status", USER_SOCKET_ROOM, 'off');
+          }
         }
       });
-      io.sockets.adapter.rooms.delete(USER_SOCKET_ROOM);
-      console.log('on_disconnection', io.sockets.adapter.rooms)
+      // console.log('on_disconnection', io.sockets.adapter.rooms)
     }
-  
-  //   users.map(user => {
-  //     if (user.connectionId && user.connectionId === socket.id) {
-  //       user.status = "off";
-  //       io.emit("changeStatus", user);
-  //     }
-  //   })
   });
 })
 
